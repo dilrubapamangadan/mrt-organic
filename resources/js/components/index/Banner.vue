2f92435d2704c58e42cf8e5e1e91bbd6f77bc711
mijurayaroth <mijurayaroth>
2021-06-23 08:39:51 +0530

    design correction

--- resources/js/components/index/Banner.vue
+++ resources/js/components/index/Banner.vue
@@ -1,34 +1,14 @@
 <template>
-    <section class="home-banner"> <img src="assets/custom/img/banner-vector.png" data-src="assets/custom/img/banner-vector.png" width="978" height="576" alt="banner-vector" class="flower"> <img src="assets/custom/img/leaf.png" data-src="assets/custom/img/leaf.png" width="370" height="306" alt="leaf" class="leaf">
+    <section class="home-banner"> <img src="assets/custom/img/banner-vector.png" data-src="assets/custom/img/banner-vector.png" width="978" height="576" 
+    alt="banner-vector" class="flower"> <img src="assets/custom/img/leaf.png" data-src="assets/custom/img/leaf.png" width="370" height="306" alt="leaf" class="leaf">
         <div class="wall"></div>
 
-<carousel  class="carousel site-banner" :autoplay="true" data-ride="carousel" data-interval="5000"  :dots="false" :responsive="{0: {items: 1,nav: false},600: {items: 2, },900: {items: 3,},1000: {items: 3,loop: false}}"  data-aos="fade-up" data-aos-duration="2500">
-        <!-- <carousel id="carouselExampleIndicators" class="carousel site-banner carousel-fade " data-ride="carousel" data-interval="8000">  -->
-            <a href="#HM-3-colum" class="skip"><i class="las la-angle-down"></i></a>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="container h-100">
-                        <div class="d-flex justify-content-left align-items-center h-100">
-                            <div class="col-6 text">
-                                <h4 class="font-normal">Reach for a healthier you</h4>
-                                <h2 class="font-bold">A Sustainable
-                                Development
-                                Venture</h2> </div>
-                        </div>
-                    </div> <img src="assets/custom/img/banner-image-1.jpg" width="1044" height="519" class=""> </div>
-                <div class="carousel-item">
-                    <div class="container h-100">
-                        <div class="d-flex justify-content-left align-items-center h-100">
-                            <div class="col-6 text">
-                                <h4 class="font-normal">Reach for a healthier you</h4>
-                                <h2 class="font-bold">A Sustainable
-                                Development
-                                Venture</h2> </div>
-                        </div>
-                    </div> <img src="assets/custom/img/banner-image-2.jpg" width="956" height="647" class=""> </div>
-            </div>
+<carousel  class="carousel site-banner carousel-fade" :autoplay="true" data-ride="carousel" data-interval="5000"  :dots="false" :responsive="{0: {items: 1,nav: false},600: {items: 2, },900: {items: 3,},1000: {items: 3,loop: false}}"  data-aos="fade-up" data-aos-duration="2500">
+       
+           
+           
         </carousel>
-
+ <a href="#HM-3-colum" class="skip"><i class="las la-angle-down"></i></a>
     </section>
 </template>
 <script>
@@ -39,3 +19,6 @@ export default {
         },
 }
 </script>
+<style scoped>
+
+</style>
