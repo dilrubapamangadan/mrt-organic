664dee4cb55747390739a5bb9471f4dba1d29309
mijurayaroth <mijurayaroth>
2021-07-10 13:11:12 +0530

    product list page

--- resources/js/components/productList.vue
+++ resources/js/components/productList.vue
@@ -1,16 +1,58 @@
 <template>
-    <section>
-        <div>
-            Product List
-        </div>
+  <div>
+    <section class="home-banner inner-banner">
+      <div class="container text-center">
+        <h2 class="font-bold page-title">Organic Products</h2>
+
+        <img
+          src="assets/custom/img/bottle-n-leaf.png"
+          width="440"
+          height="339"
+          class="bnr-organic-left"
+        />
+      </div>
+
+      <div class="wall"></div>
     </section>
+
+    <section class="category-description">
+      <div class="container">
+        <div class="box">
+          <div class="display-3 font-bold text-green">Spice Oils</div>
+          <div class="h3 text-green pb-4">Move your body , Spirit & Body</div>
+          <p>
+            MRT Organic essential oils are produced from plants that are grown
+            without the use of pesticides or herbicides in accordance with
+            organic agricultural standards. In addition, there are oils
+            distilled from wild-crafted plants.
+          </p>
+          <p>
+            A careful distillation process and Organic Agricultural standards
+            ensure the quality of Organic Essential oils are superior to Non
+            Organic Oil and its therapeutic values are excellent than
+            conventional oils.
+          </p>
+          <div class="load py-0">
+          <a href="#" class="load-all-btn"><span>Read Morea</span> <i class="las la-angle-right"></i></a>
+        </div>
+        </div>
+      </div>
+    </section>
+<section class="product-liting">
+   <div class="container">
+       <div class="row">
+
+       </div>
+   </div>
+
+</section>
+
+  </div>
 </template>
 
 <script>
-
 export default {
-        components: { 
-        }
-}
+  components: {},
+};
 </script>
 
