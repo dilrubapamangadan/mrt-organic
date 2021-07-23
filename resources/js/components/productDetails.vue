63d631f745020252e6ac1ce6fd16e34ee9f73d27
mijurayaroth <mijurayaroth>
2021-07-23 11:13:16 +0530

    detail page

--- resources/js/components/productDetails.vue
+++ resources/js/components/productDetails.vue
@@ -1,5 +1,33 @@
 <template>
   <section>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      
+      <div class="modal-body p-4 p-md-5">
+             <h3  class="font-bold text-green pb-3 mb-0">Download brochure</h3>
+
+             <div class="alert alert-info mb-4 small">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+             </div>
+         
+          <div class="form-group">
+            <input type="text" placeholder="Enter email" class="form-control">
+          </div>
+          <div class="form-group">
+            <input type="text" placeholder="Phone Number" class="form-control">
+          </div>
+     
+      <div class="d-flex">
+     
+        <button type="button" class="btn btn-default border text-dark px-4 rounded-0" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-green btn-green-sm ml-auto">Download</button>
+      </div>
+       </div>
+    </div>
+  </div>
+</div>
+
     <header1></header1>
     <section class="home-banner inner-banner">
       <div class="container-lg position-relative">
@@ -56,8 +84,8 @@
         </div>
       </div>
     </section>
-    <section class="cms ">
-      <div class="container-lg ">
+    <section class="cms">
+      <div class="container-lg">
         <h2>Floral extracts</h2>
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -96,10 +124,9 @@
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book.
           </li>
-         
         </ul>
-         <h3>Sub heading</h3>
-           <ol>
+        <h3>Sub heading</h3>
+        <ol>
           <li>
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
@@ -124,8 +151,16 @@
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book.
           </li>
-         
         </ol>
+
+        <div class="pt-4 mt-3 row">
+          <div class="col-md-6">
+            <a href="#" class="btn-green">Shop online</a>
+          </div>
+          <div class="col-md-6 text-right">
+            <a href="#" class="btn-green"  data-toggle="modal" data-target="#exampleModal">Download Brochure</a>
+          </div>
+        </div>
       </div>
     </section>
     <section class="org-product-info position-relative">
@@ -194,12 +229,15 @@
 import header1 from "./header.vue";
 import footer1 from "./footer.vue";
 import Certification from "./index/Certification.vue";
+import Index from './index.vue';
 export default {
   components: {
     header1,
     footer1,
     Certification,
+    Index,
   },
 };
+
 </script>
 
