07099d2e1243db6e312736163663989a192c7b49
dilrubapamangadan <dilrubapamangadan@gmail.com>
2021-07-29 20:16:41 +0530

    logo link

--- resources/views/layouts/adminapp.blade.php
+++ resources/views/layouts/adminapp.blade.php
@@ -61,7 +61,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{ asset('index3.html') }}" class="brand-link">
+    <a href="{{ url('/admin/dashboard') }}" class="brand-link">
       <img src="{{ asset('assets/img/logo.png') }}"
            alt="AdminLTE Logo"
            class="brand-image img-circle elevation-3"
